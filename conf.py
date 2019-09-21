73f9cce215a36a414e6c4c34bd60bfa3bfc4329c
Abhijit Tudu <abhijittudu98@gmail.com>
2019-09-21 16:40:47 +0530

    anomaly detection
    IoT & ML: Anolomy Detection and alert on Telegram Channel by Python.

---
+++ conf.py
@@ -0,0 +1,6 @@
+telegram_chat_id = "This is your id of Telegram Channel"
+telegram_bot_id = "This is your Telegram Bot's ID"
+API_KEY = 'This is your Bolt Cloud account API key'
+DEVICE_ID = 'This is the ID of your Bolt device'
+FRAME_SIZE = 10
+MUL_FACTOR = 6
